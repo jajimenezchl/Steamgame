9541ea4eda4905e7acb291f0e455a4547d429e92
jajimenezchl <ja.jimenezchl@gmail.com>
2022-03-04 17:16:58 -0300

    creacion archivo Api

---
+++ src/Components/Api.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+
+class Api extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://gbfs.citibikenyc.com/gbfs/en/station_information.json', { mode: 'no-cors' })
+      .then(
+        (res) => res.json(),
+      )
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.items,
+          })
+        },
+        // Nota: es importante manejar errores aquí y no en
+        // un bloque catch() para que no interceptemos errores
+        // de errores reales en los componentes.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          })
+        },
+      )
+      .then((data) => {
+        resolve(data ? JSON.parse(data) : {})
+      })
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state
+
+    if (error) {
+      return (
+        <div>
+          Error:
+          {error.message}
+        </div>
+      )
+    } if (!isLoaded) {
+      return <div>Loading...</div>
+    }
+    return (
+      <div>Loading...</div>
+
+    )
+  }
+}
+
+export default Api

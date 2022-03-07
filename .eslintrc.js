f773fca504883ab2ce14549414d0889ad75729bd
jajimenezchl <ja.jimenezchl@gmail.com>
2022-03-07 03:59:59 -0300

    se agregan imagenes y usos de api Steam

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,18 @@ module.exports = {
   rules: {
     'linebreak-style': 0,
     semi: [2, 'never'],
+    'no-console': 'off',
+    "jsx-a11y/label-has-associated-control": ["error", {
+      "required": {
+        "some": ["nesting", "id"]
+      }
+    }],
+    "jsx-a11y/label-has-for": ["error", {
+      "required": {
+        "some": ["nesting", "id"]
+      }
+    }]
+  },
   },
 
 }
--- src/Components/Api.jsx
+++ src/Components/Api.jsx
@@ -7,24 +7,41 @@ class Api extends React.Component {
       error: null,
       isLoaded: false,
       items: [],
+      num: '',
+      resultado: [],
+      nombre : '',
+      listItems : []
     }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.getDetail = this.getDetail.bind(this);
   }
 
   componentDidMount() {
-    fetch('https://gbfs.citibikenyc.com/gbfs/en/station_infordsadsadmation.json', { mode: 'no-cors' })
+    const {
+      items, num, nombre
+    } = this.state
+
+    fetch('http://api.steampowered.com/ISteamApps/GetAppList/v2')
       .then(
         (res) => res.json(),
       )
       .then(
         (result) => {
+          // console.log(result.applist.apps.length)
+          //console.log(result.applist.apps)
+          // console.log('test name',
+          //   (result.applist.apps.filter(
+          //   (item) => 
+          //   item.name.toString().toLowerCase().indexOf(nombre.toString().toLowerCase()) > -1)
+          //    || null))
+
           this.setState({
             isLoaded: true,
-            items: result.items,
+            items: result.applist.apps,
+
           })
         },
-        // Nota: es importante manejar errores aquí y no en
-        // un bloque catch() para que no interceptemos errores
-        // de errores reales en los componentes.
         (error) => {
           this.setState({
             isLoaded: true,
@@ -34,9 +51,82 @@ class Api extends React.Component {
       )
   }
 
-  render() {
-    const { error, isLoaded, items } = this.state
+  // imagenes de Steam
+  // https://cdn.cloudflare.steamstatic.com/steam/apps/757580/header.jpg 654970
 
+  handleChange(event) {
+    this.setState({
+      nombre: event.target.value,
+    
+    })
+  }
+
+  handleSubmit(event) {
+
+    event.preventDefault()
+    this.getDetail()
+    this.setState({
+        resultado: (this.state.items.filter(
+        (item) => 
+        item.name.toString().toLowerCase().indexOf(this.state.nombre.toString().toLowerCase()) > -1)),
+
+    })
+   
+  }
+
+  getDetail(id){
+    fetch(`https://store.steampowered.com/api/appdetails?appids=${id}`)
+      .then(
+        (res) => res.json(),
+      )
+      .then(
+        (result) => {
+          console.log('detalle: ',result)
+          return result
+        },
+        (error) => {
+          console.log('error Detalle: ',error)
+        },
+      )
+  }
+
+  render() {
+    const {
+      error, isLoaded, num, resultado,items,nombre
+    } = this.state
+
+    
+
+    const src = (
+      num !== '' && resultado &&  resultado.length == 1
+        ? `https://cdn.cloudflare.steamstatic.com/steam/apps/${num}/header.jpg`
+        : '')
+
+     //console.log(resultado)
+    //console.log('resultado:', resultado)
+
+    const listItem = (
+      resultado !== null
+      ?  resultado.slice(0, 5).map((result) =>
+      
+      <>
+        {console.log(this.getDetail(result.appid))}
+        <div key={result.appid} class="card p-3" style={{width: 2 + 'rem'}}>
+          <img class="card-img-top" src={`https://cdn.cloudflare.steamstatic.com/steam/apps/${result.appid}/header.jpg`} alt="Card image cap" />
+          <div class="card-body">
+            <h5 class="card-title">{result.name}</h5>
+             {/* <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+            <a href={`https://store.steampowered.com/agecheck/app/${result.appid}/`} class="btn btn-primary">Tienda</a> 
+          </div>
+        </div>
+        
+      </>
+      ) 
+      : <li>sin resultados</li>
+      )
+    
+
+      
     if (error) {
       return (
         <div>
@@ -48,8 +138,23 @@ class Api extends React.Component {
       return <div>Loading...</div>
     }
     return (
-      <div>{items}</div>
 
+      <>
+
+      <nav class="navbar navbar-light bg-dark">
+        <a class="navbar-brand text-light font-weight-bold" href="#">STEAM LIST</a>
+    
+          <form onSubmit={this.handleSubmit} class="form-inline my-2 my-lg-0">
+            <input class="form-control" type="text" value={nombre} onChange={this.handleChange} />
+            <button type="submit" value="Submit" class="btn btn-secondary my-2 my-sm-0">Buscar</button> 
+          </form>
+     
+      </nav>
+      
+      <div class="card-group">
+      {listItem}
+      </div>
+      </>
     )
   }
 }
